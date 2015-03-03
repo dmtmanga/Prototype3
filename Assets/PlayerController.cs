2e9e02dd7fdcda5a21410b3a25dc1fccf94ea0ab
Matt Talubas <dmtmanga@gmail.com>
2015-03-03 15:36:20 -0500

    Publish

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float force = 5f;
+	public KeyCode up;
+	public KeyCode down;
+	public KeyCode fire;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Movement
+		if (Input.GetKey ("up")) {
+			rigidbody2D.AddForce( new Vector2(0, force) );
+		}
+		else if (Input.GetKey ("down")) {
+			rigidbody2D.AddForce( new Vector2(0, -1*force) );
+		}
+
+	}
+}
