971d98942a396130162f526a6075a98ba1ba9984
Matt Talubas <dmtmanga@gmail.com>
2015-03-04 03:47:34 -0500

    some more tweaks

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	private GameObject _GM;
+
+	// Use this for initialization
+	void Start () {
+		_GM = GameObject.FindGameObjectWithTag ("GameController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > 10f || transform.position.y > 10f)
+			Destroy (gameObject);
+	}
+
+	void OnTriggerEnter2D ( Collider2D collider ) {
+		if (collider.tag == "Player") {
+			Destroy (gameObject);
+			_GM.SendMessage("Score", collider.name);
+		}
+	}
+	
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Text p1ScoreUI;
+	public Text p2ScoreUI;
+
+	private int _p1Score = 0;
+	private int _p2Score = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		p1ScoreUI.text = _p1Score.ToString ();
+		p2ScoreUI.text = _p2Score.ToString ();
+	}
+
+	void Score ( string player ) {
+		if (player == "P1")
+			_p2Score++;
+		else if (player == "P2")
+			_p1Score++;
+	}
+
+}
